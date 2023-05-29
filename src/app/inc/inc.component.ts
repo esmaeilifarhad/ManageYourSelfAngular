d927247fd56d69b7f96a8b1a1fda7d3aef615a36
f.esmaeili <f.esmaeili@enb.local>
2023-05-29 08:27:58 +0330

    s

--- src/app/inc/inc.component.ts
+++ src/app/inc/inc.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-inc',
   templateUrl: './inc.component.html',
   styleUrls: ['./inc.component.css']
 })
-export class IncComponent {
+export class IncComponent implements OnInit,AfterViewInit {
 add:number=0;
+  @ViewChild('par') par!: ElementRef ;
 
+public ngOnInit(){
+}
+public ngAfterViewInit(): void {
+  this.par.nativeElement.innerHtml = "123456"
+}
 public increment(){
   this.add++;
 }
